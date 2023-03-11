1bd196f3db9ddccd037122a79b3d1ebee9b11192
Worm <mwormley008@gmail.com>
2023-03-10 19:58:08 -0600

    Further commit

--- BlackJack.py
+++ BlackJack.py
@@ -5,7 +5,11 @@ import random
 table = []
 
 class Game:
-    # Ok so this game is going to need 2 players, a dealer and a player, a deck of cards, and rules I guess?
+    def change_turn(self):
+        if turn_count == 0:
+            turn_count = 1
+        else:
+            turn_count = 0
     pass
 
 
@@ -14,7 +18,7 @@ class Deck:
     def __init__(self):
         self.cards = []
         suits = ['hearts', 'diamonds', 'clubs', 'spades']
-        values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+        values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '1']
 
         for suit in suits:
             for value in values:
@@ -39,14 +43,21 @@ class Human:
     # Each human will have a hand of cards and the ability to hit or stay (unless the dealer doesn't have that ability)
     def __init__(self, name):
         self.hand = []
+        self.handvalue = 0
         self.name = name
 
     def hit(self, deck):
         #human is passed a card, I think I want to generate the card as the the top of the deck
         card = deck.cards.pop(0)
         self.hand.append(card)
+        self.handvalue += int(card.value)
+        # Calculate if the player has hit 21 or has gone bust
+        
+        
 
     def stay(self):
+        # the player needs the option to stay so that they can stop hitting but the dealer doesn't need that method I am pretty sure
+        # this will also pass the turn
         pass
 
     def __str__(self):
@@ -58,10 +69,16 @@ class Dealer(Human):
     def __init__(self, name):
         super().__init__(name)  # Call the __init__ method of the parent class
         table.append(self)
-    # def deal(deck):
-    #     for seated at table:
-    #         seated.hit(deck)
-    pass
+    
+    # One thing I will need to figure out is how to hide the second value of the card 
+    def deal(self, deck):
+        for seated in table:
+            seated.hit(deck)
+        for seated in table:
+            seated.hit(deck)
+    
+    def see_dealer_hand(self):
+        print(f"The dealer is showing a {str(self.hand[0])}.")
 
     def __str__(self):
         return self.name
@@ -72,23 +89,38 @@ class Player(Human):
         super().__init__(name)  # Call the __init__ method of the parent class
         table.append(self)
 
+    def hit(self, deck):
+        #human is passed a card, I think I want to generate the card as the the top of the deck
+        card = deck.cards.pop(0)
+        self.hand.append(card)
+        self.handvalue += int(card.value)
+        print(card)
+        print(self.handvalue)
+        if self.handvalue > 21:
+            print("You have gone bust.")
+        elif self.handvalue == 21:
+            print("You have won the game.")
+
 def main():
     #This defines the main function of the program
     if __name__=="__main__":
-        print("Hello, world!")
-        #Shuffle the cards when the game begins
+        Jeff, George = Player('Jeff'), Dealer('George')
+        turn = 0
+        main_deck = Deck()
+        main_deck.shuffle()
+        print([str(card) for card in main_deck.cards])
+        for seated in table:
+            print(str(seated))
+        # Jeff.hit(main_deck)
+        # Jeff.hit(main_deck)
+        George.deal(main_deck)
+        print([str(card) for card in Jeff.hand])
+        # print([str(card) for card in George.hand])
+        print([str(card) for card in main_deck.cards])
+        George.see_dealer_hand()
+        # I'm going to start programming a game here because I don't really want to ask for inputs I'd rather do a gui but we'll see
         
-
+        Jeff.hit(main_deck)
+        
 main()
         
-Jeff, George = Player('Jeff'), Dealer('George')
-main_deck = Deck()
-main_deck.shuffle()
-print([str(card) for card in main_deck.cards])
-for seated in table:
-    print(str(seated))
-Jeff.hit(main_deck)
-Jeff.hit(main_deck)
-
-print([str(card) for card in Jeff.hand])
-print([str(card) for card in main_deck.cards])
